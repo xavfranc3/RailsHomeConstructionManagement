ecca3a9ceb6bed291714b1a6790b1a20d093c61b
Xav <xavfranc3@gmail.com>
2022-03-09 14:23:41 +0100

    Initial commit

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Inside app/javascript/application.js
+import "@hotwired/turbo-rails"
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+})
