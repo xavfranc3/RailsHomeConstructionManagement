a4a88bccc5b437ef0d9dd8789e24aa83eb264532
Xav <xavfranc3@gmail.com>
2022-03-15 22:45:42 +0100

    Add/Seeders

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 AppAdmin.find_or_create_by(name: 'AppAdmin') do |user|
   user.name = 'AppAdmin'
-  user.email = 'app_admin@app.com'
+  user.email = 'admin@test.com'
   user.password = 'Password123'
+end
+
+Tenant.find_or_create_by(name: 'Tenant') do |tenant|
+  tenant.name = 'TestTenant1'
+  tenant.creator_id = 1
 end
